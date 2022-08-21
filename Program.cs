aaca19bc2a58a94e8cf48cf566effc68b16d8b53
runson <runson@example.com>
2022-08-21 17:04:01 +0545

    lab1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ClassAndObject
+{
+
+    public class Testing
+    {
+
+        public void Checking()
+        {
+            Console.WriteLine("Hello Rishek");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Testing obj = new Testing(); 
+            obj.Checking();
+        }
+    }
+}
